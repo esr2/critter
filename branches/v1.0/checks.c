5e42a2e8a8f1c493f2466bc84500151ca529100d
erosenba <erosenba@b4bc5322-c34b-38d7-5276-7cbb6342716e>
2011-01-08 04:06:26 +0000

    seperated v1.0 into its own branch

---
+++ branches/v1.0/checks.c
@@ -0,0 +1,51 @@
+/*
+ *  checks.c
+ *  
+ *
+ *  Created by Erin Rosenbaum on 11/22/10.
+ *  Copyright 2010 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#include "checks.h"
+#include <stdio.h>
+
+void ifHasBraces(enum tree_code statementValue, YYLTYPE location) {
+	if (statementValue != COMPOUND_STATEMENT) {
+		lyyerror(location, "Please use braces after all if statements");
+	}
+}
+
+void isFunctionTooLong(YYLTYPE location) {
+	int MAX_FUNCTION_LENGTH = 100;
+	
+	if (location.last_line - location.first_line + 1 >= MAX_FUNCTION_LENGTH) {
+		lyyerror(location, "Function is too long");
+	}
+}
+
+void tooManyParameters(YYLTYPE location) {
+	int MAX_NUM_PARAMETERS = 7;
+	
+	static int numParameters = -1;
+	static YYLTYPE prevLoc;
+	
+	if (location.first_line != prevLoc.first_line || numParameters == -1) {
+		if (numParameters != -1 && numParameters >= MAX_NUM_PARAMETERS) {
+			char string[200];
+			sprintf(string,
+					"Please use less than %d function parameters, you used %d",
+					MAX_NUM_PARAMETERS, numParameters);
+			lyyerror(prevLoc, string);
+		}
+		
+		numParameters = 1;
+	}
+	
+	numParameters++;
+	prevLoc = location;
+}
+
+void CPlusPlusComments(YYLTYPE location) {
+	lyyerror(location, "Don't use c++ style comments");
+}
---
+++ branches/v1.0/checks.h
@@ -0,0 +1,21 @@
+/*
+ *  checks.h
+ *  
+ *
+ *  Created by Erin Rosenbaum on 11/22/10.
+ *  Copyright 2010 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#ifndef CHECKS_INCLUDED
+#define CHECKS_INCLUDED
+
+#include "tree.h"
+#include "c.tab.h"
+
+void ifHasBraces(enum tree_code statementValue, YYLTYPE location);
+void isFunctionTooLong(YYLTYPE location);
+void tooManyParameters(YYLTYPE location);
+void CPlusPlusComments(YYLTYPE location);
+
+#endif
