fb0e7fa068a8fcfda89c34c92281d28a8e0b1315
erosenba <erosenba@b4bc5322-c34b-38d7-5276-7cbb6342716e>
2011-02-13 23:28:27 +0000

    added hook level begin/end program/file to manage new functionCall DynArrays. Got a stack of function calls working with the right location. Unfortunately it really needs to be called in the reverse order from the stack...

--- trunk/hooks.c
+++ trunk/hooks.c
@@ -9,18 +9,78 @@
 
 #include "hooks.h"
 #include "sax.h"
+#include "dynarray.h"
+#include <stdlib.h>
+#include <strings.h>
+#include <stdio.h>
+#include <assert.h>
 
-static int areLocationsAdjacent(YYLTYPE one, YYLTYPE two) {
-	if (!one.filename || !two.filename) { return -1; }
-	if (strcmp(one.filename, two.filename) != 0) { return 0; }
-	
-	if ((one.first_line - two.last_line <= 1) || 
-		(two.first_line - one.last_line <= 1)) { return 1; }
+static DynArray_T functionCallsArray;
+static DynArray_T locationsArray;
+
+/**
+ * Called at the beginning of each file before parsing 
+ * begins.
+ */
+void h_beginFile(char* filename) {
+	beginFile(filename);
 }
 
+/**
+ * Called at the end of each file. Location.first_line 
+ * = last_line.
+ */
+void h_endFile(YYLTYPE location) {
+	endFile(location);
+}
 
-static YYLTYPE lastDirectDeclarator;
-static YYLTYPE firstDeclarationSpecifier;
+/**
+ * Called at the beginning of the program execution before
+ * parsing begins.
+ */
+void h_beginProgram() {
+	functionCallsArray = DynArray_new(0);
+	locationsArray = DynArray_new(0);
+	
+	assert(functionCallsArray != NULL);
+	assert(locationsArray != NULL);
+	beginProgram();
+}
+
+static void freeLocations(void* element, void* extra) { 
+	YYLTYPE *location = (YYLTYPE *)element;
+	if (location->filename != NULL) { free(location->filename); }
+	free(element); 
+}
+
+static YYLTYPE* allocateLocation(YYLTYPE location) {
+	YYLTYPE *loc = malloc(sizeof(YYLTYPE));
+	loc->filename = malloc(sizeof(char) * strlen(location.filename));
+	strcpy(loc->filename, location.filename);
+	loc->first_line = location.first_line;
+	loc->first_column = location.first_column;
+	loc->last_line = location.last_line;
+	loc->last_column = location.last_column;
+	
+	return loc;
+}
+
+/**
+ * Called at the end of the program execution.
+ */
+void h_endProgram(YYLTYPE location) {
+	endProgram(location);
+	
+	assert(functionCallsArray != NULL);
+	assert(locationsArray != NULL);
+	
+	// Free the function arrays
+	DynArray_map(locationsArray, freeLocations, NULL);
+	DynArray_free(locationsArray);
+	DynArray_free(functionCallsArray);
+}
+
+/*---------------------------------------------*/
 static int inDeclarator = 0;
 
 void h_registerIdentifier(YYLTYPE location) {
@@ -28,12 +88,18 @@ void h_registerIdentifier(YYLTYPE location) {
 }
 
 void h_beginDirectDeclarator(YYLTYPE location) {
+	assert(functionCallsArray != NULL);
+	assert(locationsArray != NULL);
+	
 	inDeclarator++;
+	DynArray_add(functionCallsArray, (void*)beginDeclarator);
+	DynArray_add(locationsArray, allocateLocation(location));
 }
 
 void h_endDirectDeclarator(YYLTYPE location) {
 	inDeclarator--;
-	lastDirectDeclarator = location;
+	DynArray_add(functionCallsArray, (void*)endDeclarator);
+	DynArray_add(locationsArray, allocateLocation(location));
 }
 
 static void h_beginDeclarationSpecifiers(YYLTYPE location) {
@@ -46,6 +112,21 @@ static void h_endDeclarationSpecifiers(YYLTYPE location) {
 
 void h_beginFunctionDefinition(YYLTYPE location) {
 	beginFunctionDefinition(location);
+	int i;
+	void (*func)(YYLTYPE);
+	
+	for (i = 0; i < 10; i++) {
+		int len = DynArray_getLength(locationsArray) - 1;
+		YYLTYPE* loc = DynArray_removeAt(locationsArray, len);
+		assert(loc != NULL);
+		
+		func = DynArray_removeAt(functionCallsArray, len);
+		assert(func != NULL);
+	
+		(*func)(*loc);
+		freeLocations(loc, NULL);
+	}
+	
 	/* send appropriate calls for declaration_specifier, declarator etc */
 	
 }
@@ -53,7 +134,7 @@ void h_beginFunctionDefinition(YYLTYPE location) {
 
 /* Type specifiers: */
 static void h_registerTypeSpecifier(YYLTYPE location) {
-	if (!inDeclarator) firstDeclarationSpecifier = location;
+	
 }
 
 void h_registerVoid(YYLTYPE location) {h_registerTypeSpecifier(location);}
