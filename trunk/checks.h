f432b71785efe3ea7c63492537d0c157a8ed788c
erosenba <erosenba@b4bc5322-c34b-38d7-5276-7cbb6342716e>
2011-02-23 23:30:16 +0000

    added tooManyFunctionsInFile check

--- trunk/checks.h
+++ trunk/checks.h
@@ -29,6 +29,7 @@ void globalHasComment(YYLTYPE location, int progress);
 void isLoopTooLong(YYLTYPE location);
 void isLoopEmpty(YYLTYPE location, void (*beginLoop)(YYLTYPE), char* loopType);
 void isCompoundStatementEmpty(YYLTYPE location, int progress);
+void tooManyFunctionsInFile(YYLTYPE location, int progress);
 
 enum {
 	END = 1,
