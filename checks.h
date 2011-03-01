a8096cd4f366cfec62442a28f19248495abaaf46
erosenba <erosenba@b4bc5322-c34b-38d7-5276-7cbb6342716e>
2011-03-01 01:14:21 +0000

    fixed isVariableNameTooShort to only error inside declarations and to print out the offending variable/function name

--- checks.h
+++ checks.h
@@ -23,7 +23,7 @@ void switchCasesHaveBreaks(YYLTYPE location, int progress, int isCase);
 void tooDeeplyNested(YYLTYPE location, int progress);
 void useEnumNotConstOrDefine(YYLTYPE location, int progress);
 void neverUseGotos(YYLTYPE location);
-void isVariableNameTooShort(YYLTYPE location, char* identifier);
+void isVariableNameTooShort(YYLTYPE location, int progress, char* identifier);
 void isMagicNumber(YYLTYPE location, int progress, char* constant);
 void globalHasComment(YYLTYPE location, int progress);
 void isLoopTooLong(YYLTYPE location);
