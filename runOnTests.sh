fd6a758bf7051cc8a8d2530346ce7d5d7991f44a
erin.s.rosenbaum <erin.s.rosenbaum@b4bc5322-c34b-38d7-5276-7cbb6342716e>
2011-08-18 06:58:27 +0000

    fixed runOnTests.sh so that its compatible with the new repository structure

--- runOnTests.sh
+++ runOnTests.sh
@@ -8,7 +8,7 @@ PATHS=(
  "/Users/erinrosenbaum/Documents/Senior_Year/Thesis/ish/spr02/student_13"
  )
 
-PROGRAM="/Users/erinrosenbaum/Documents/Senior_Year/Thesis/repository/trunk/critTer"
+PROGRAM="/Users/erinrosenbaum/Documents/Senior_Year/Thesis/critTer/trunk/critTer"
 
 echo $(date) >&2
 
